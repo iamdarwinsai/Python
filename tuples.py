3454851e46deafbb727b978f99c36bb84b1e9066
XPLOIT3R <38688596+xp1oit3r@users.noreply.github.com>
2022-02-06 16:42:16 +0530

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,24 @@
+'''
+Tuples
+
+* immutable
+* values stored in tuple cannot be changed
+
+'''
+'''
+
+joey=()
+print(type(joey))
+
+'''
+
+'''
+some tuple functions
+
+length
+min
+max
+sum
+count
+
+'''
