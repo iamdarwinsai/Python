208dc5cdb17f7b128c186b31047e909d2f81b96c
XPLOIT3R <38688596+xp1oit3r@users.noreply.github.com>
2022-02-06 16:29:25 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,65 @@
+'''
+Topic : Lists []
+
+* Mutable
+* it stores different types
+* Values in the list are seperated  commas,
+
+'''
+
+# joey=[]
+# print(type(joey))
+
+# joey=[1,5,'python',34,54,344,5543,213]
+
+# print(joey[0:5:2])  #[start:stop:jump]
+
+
+'''
+list crud operations
+c - create
+r - read
+u - update
+d - delete
+
+List functions
+append - adds at the end (entire list appends in the form of list)
+extend - to add more then 1 values(entire lists append in to the values)
+insert - to insert at particular location
+pop - we'll remove the element with the help of index
+remove - we'll remove the element with the help of value
+copy - copies the values
+sort - sorts the value from small to large
+length - prints the lenth
+count - counts the elements in the list
+sum - adds the element
+index - shows the element present at the index
+minimum - print the highets elemnt
+maximum - prints the lowest element
+ 
+'''
+
+joey=[1,2,4,5,3,1,5,5,5]
+# joey.append(7)
+# joey.extend([1,2])
+# joey.insert(0,'abcd')
+# joey.pop(1)
+# joey.remove(2)
+'''
+a=joey.index(1)
+print(a)
+'''
+# joey.sort()
+# joey.len()
+'''
+a=joey.count(5)
+print(a)
+'''
+
+# print(max(joey))
+# print(min(joey))
+# print(sum(joey))
+# print(len(joey))
+
+
+
