dc09cbc5e6f69fb0c940e90f3a201a94c66e8fda
XPLOIT3R <38688596+xp1oit3r@users.noreply.github.com>
2022-02-06 07:37:55 +0530

    Add files via upload

---
+++ typeconversion.py
@@ -0,0 +1,14 @@
+# 2 types of conversion Implicit(automatic), Explicit(forece)
+
+#implicit
+joey=99
+chandler=float(joey)
+print(type(chandler))
+
+#explicit
+a=99.11
+b=int(a)
+print(b)
+
+
+# we cant change str to int or float
