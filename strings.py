291301a32f552ad2da4b1dd15e4adcd2671f2d72
XPLOIT3R <38688596+xp1oit3r@users.noreply.github.com>
2022-02-07 09:50:34 +0530

    strings basics and its functions

---
+++ strings.py
@@ -0,0 +1,62 @@
+'''
+Strings = Collection of alphabets or collection of characters
+
+Its functions
+
+capitalize - Makes the first letter Capital
+casehold - it prints every letter in small
+centre - perform pracricall you'll understand
+coount - counts the words
+endswith - to verify its ending with the given word
+startswith - to verify its starting with the given word 
+find - shows the position(index number)
+index - same as index, but the difference between find and index is, in index if the word mentioned isnt present it shows error whereas in find it shows -1
+isalnum - the string should be either alpha or numeric
+isnumeric - string should be numeric or else returns false
+isalpha - string should be alpha or else returns false
+upper - prints whole string in upper case
+lower - prints whole string in lower case
+strip -it removes the empty spaces from startig or ending of the whole sentence
+rstrip - removes empty spaces after the ending of the sentence
+lstrip - removes the spsaces before the starting if the sentence
+split - it converts string into list.
+replace - to change the old word with a new word in the sentence
+removeprefix - to remove the prefix of the word in the string
+removesuffix - to remove the suffix of the word in the string
+format - preform practically
+'''
+
+
+
+# joey= 'this is a this'
+'''
+a=joey.capitalize()
+print(a)
+'''
+# a=joey.casefold()
+# a=joey.center(12,'#')
+# a=joey.count('this')
+# a=joey.endswith('this')
+# a=joey.find('a')
+'''
+joey="                         hello joey          "
+a=joey.strip()
+print(a)
+'''
+'''
+joey= "How You Doing?"
+a=joey.split()
+print(a)
+
+'''
+# a=str(input("enter your name :\n"))
+# joey = 'hello brother {}'.format(a)
+# print(joey)
+
+
+# a="this is a box"
+# joey=a.split()
+# joey[1]='are'
+# chandler=' '.join(map(str,joey))
+# print(chandler)
+
