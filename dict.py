8964cd73156c3f57a97f54577518ffad3e298fb6
XPLOIT3R <38688596+xp1oit3r@users.noreply.github.com>
2022-02-07 10:36:33 +0530

    Dictonary Basics and its functions

---
+++ dict.py
@@ -0,0 +1,45 @@
+'''
+Dict {key:value}
+
+Its a mutuable datatype
+also called as mapping data type
+it contains keys and values
+
+its functions
+clear - makes an empty set
+get - to get the data from the key
+items - prints the whole items in the dicts
+keys - prints the keys
+values - prints the values
+
+
+'''
+
+joey={'name':'joey','age':2,'phone':9876789876}
+# joey.clear()
+# a=joey.get('name')
+# a=joey.items()
+# a=joey.keys()
+# a=joey.values()
+# print(a)
+
+'''
+how to print keys and values using for loop
+for i,j in joey.items():
+    print(j)
+'''
+
+'''
+nested dictionary
+
+joey={
+        'names': {
+            'name':'joey',
+        },'rajesh':'manish'    
+
+}
+a=joey.values()
+print(a)
+
+'''
+
